9f865efb4fbb2a4c2a525bb964f63ece680edc03
MaxMercer <mr.maxmercer@gmail.com>
2019-01-15 09:57:05 -0500

    fun

---
+++ ProjectMETA/src/ProjectMETA.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : ProjectMETA.cpp
+// Author      : MaxMercer
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string j;
+	cout << "What is your job" << endl;
+	cin >> j;
+
+	cout << "Your job is " << j << endl; // prints !!!Hello World!!!
+	return 0;
+}
